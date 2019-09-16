7e4e77b5d7e31b13b037e8b8bdf27b8b71317042
eshamster <hamgoostar@gmail.com>
2019-09-16 04:42:37 +0000

    Add: Calculations for tone and octave

--- js/tone.lisp
+++ js/tone.lisp
@@ -2,29 +2,66 @@
   (:use :cl
         :ps-experiment
         :parenscript)
- (:export :get-tone-freq))
+  (:export :get-tone-freq
+           :tone-to-number
+           :number-to-tone
+           :calc-tone-by-diff))
 (in-package :proto-cl-harmony/js/tone)
 
 (enable-ps-experiment-syntax)
 
 ;; --- tone --- ;;
 
+(defvar.ps+ *tone-name-list*
+    '((:a) (:a+ :b-)
+      (:b)
+      (:c) (:c+ :d-)
+      (:d) (:d+ :e-)
+      (:e)
+      (:f) (:f+ :g-)
+      (:g) (:g+ :a-)))
+
 (defun.ps+ get-tone-freq (tone octave)
   (let ((base-freq (gethash tone *tone-table*)))
     (assert base-freq)
     (* base-freq (expt 2 octave))))
 
+(defun.ps+ calc-tone-by-diff (tone octave diff &optional (prefer-sharp-p t))
+  "Calculate tone and octave.
+The \"diff\" is number of half-tone from base \"tone\" and \"octave\"."
+  (number-to-tone (+ (tone-to-number tone octave)
+                     diff)
+                  prefer-sharp-p))
+
+(defun.ps+ tone-to-number (tone octave)
+  "Convert tone and octave to number.
+The number 0 represents 440Hz A (= tone :a and octave 0).
+Increment it whenver half-tone is added.
+Decrement it whenver half-tone is subtracted."
+  (dotimes (i (length *tone-name-list*))
+    (when (find tone (nth i *tone-name-list*))
+      (return-from tone-to-number (+ i (* 12 octave)))))
+  (error "The tone name \"~A\" is invalid" tone))
+
+(defun.ps+ number-to-tone (number &optional (prefer-sharp-p t))
+  "Convert number to tone and octave.
+See the document of tone-to-number for detail."
+  (let* ((octave     (floor (/ number 12)))
+         (tone-index (mod number 12))
+         (tone-list  (nth tone-index *tone-name-list*)))
+    (assert tone-list)
+    (values (if (= (length tone-list) 2)
+                (if prefer-sharp-p
+                    (car tone-list)
+                    (cadr tone-list))
+                (car tone-list))
+            octave)))
+
 (defun.ps+ make-tone-table ()
   (let ((table (make-hash-table))
         (freq 440)
         (scale-factor (expt 2 1/12)))
-    (dolist (tones '((:a) (:a+ :b-)
-                     (:b)
-                     (:c) (:c+ :d-)
-                     (:d) (:d+ :e-)
-                     (:e)
-                     (:f) (:f+ :g-)
-                     (:g) (:g+ :a-)))
+    (dolist (tones *tone-name-list*)
       (dolist (tone tones)
         (setf (gethash tone table) freq))
       (setf freq (* freq scale-factor)))
