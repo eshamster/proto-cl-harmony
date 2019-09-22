f0a5d28628a5ba54259d30336d0afd2e2328b344
eshamster <hamgoostar@gmail.com>
2019-09-22 07:57:45 +0000

    Add: Functions about measure

--- js/sequencer.lisp
+++ js/sequencer.lisp
@@ -7,7 +7,9 @@
            :init-sequencer
            :start-sequencer
            :register-note-list
-           :get-quater-note-tick)
+           :get-quater-note-tick
+           :calc-last-measure
+           :calc-notes-in-measure)
   (:import-from :proto-cl-harmony/js/tone
                 :get-tone-freq))
 (in-package :proto-cl-harmony/js/sequencer)
@@ -29,7 +31,9 @@
 (defstruct.ps+ sequencer
     audioctx
   (t0 0) ; time when starting sequencer
-  (note-queue (list)))
+  (note-queue (list))
+  (beat-count 4)
+  (beat-base 4))
 
 ;; --- interface --- ;;
 
@@ -67,6 +71,39 @@
              (osc.stop  (+ t0 (tick-to-sec (+ start-tick resume-tick) bpm)))))))
      *monitor-ms*)))
 
+;; - beat and measure - ;;
+
+(defun.ps+ set-beat (sequencer count base)
+  (setf (sequencer-beat-count sequencer) count
+        (sequencer-beat-base  sequencer) base))
+
+(defun.ps+ calc-last-measure (sequencer)
+  (let ((last-tick 0))
+    (dolist (note (peek-all-notes (sequencer-note-queue sequencer)))
+      (let ((tail-tick (1- (+ (note-start-tick  note)
+                              (note-resume-tick note)))))
+        (when (> tail-tick last-tick)
+          (setf last-tick tail-tick))))
+    (1+ (floor (/ last-tick
+                  (get-tick-per-measure (sequencer-beat-count sequencer)
+                                        (sequencer-beat-base  sequencer)))))))
+
+(defun.ps+ calc-notes-in-measure (sequencer measure)
+  (when (> measure (calc-last-measure sequencer))
+    (return-from calc-notes-in-measure (list)))
+  (multiple-value-bind (measure-start-tick measure-end-tick)
+      (get-tick-range-in-measure (sequencer-beat-count sequencer)
+                                 (sequencer-beat-base  sequencer)
+                                 measure)
+    (let ((result-notes (list)))
+      (dolist (note (peek-all-notes (sequencer-note-queue sequencer)))
+        (let* ((start-tick (note-start-tick note))
+               (end-tick   (+ start-tick (note-resume-tick note))))
+          (when (overlap-range-p start-tick end-tick
+                                 measure-start-tick measure-end-tick)
+            (push note result-notes))))
+      (reverse result-notes))))
+
 ;; --- internal --- ;;
 
 ;; - tick - ;;
@@ -80,6 +117,16 @@
 (defun.ps+ get-tick-per-sec (bpm)
   (let ((tick-per-min (* bpm *quater-note-tick*)))
     (/ tick-per-min 60)))
+
+(defun.ps+ get-tick-per-measure (beat-count beat-base)
+  (* beat-count
+     (/ (* (get-quater-note-tick) 4)
+        beat-base)))
+
+(defun.ps+ get-tick-range-in-measure (beat-count beat-base measure)
+  (let ((tick-per-measure (get-tick-per-measure beat-count beat-base)))
+    (values (* tick-per-measure measure)
+            (* tick-per-measure (1+ measure)))))
 
 ;; - queue - ;;
 
@@ -108,3 +155,14 @@
                    (rec (push (pop queue) result))
                    result))))
     (rec (list))))
+
+(defun.ps+ peek-all-notes (queue)
+  queue)
+
+;; - misc - ;;
+
+(defun.ps+ overlap-range-p (min1 max1 min2 max2)
+  "Judge if [min1, max1) and [min2, max2) overlap."
+  (not (or (<= max1 min2)
+           (<= max2 min1))))
+
